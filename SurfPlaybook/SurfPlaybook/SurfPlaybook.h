6f72f9cbd47fe797a26aed9ccadd1fd6f1c2a0f5
Александр Чаусов <chausov@surfstudio.ru>
2021-04-13 15:27:51 +0300

    created framework project, added gitignore

---
+++ SurfPlaybook/SurfPlaybook/SurfPlaybook.h
@@ -0,0 +1,18 @@
+//
+//  SurfPlaybook.h
+//  SurfPlaybook
+//
+//  Created by Александр Чаусов on 13.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SurfPlaybook.
+FOUNDATION_EXPORT double SurfPlaybookVersionNumber;
+
+//! Project version string for SurfPlaybook.
+FOUNDATION_EXPORT const unsigned char SurfPlaybookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurfPlaybook/PublicHeader.h>
+
+
