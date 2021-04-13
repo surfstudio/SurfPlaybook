bd78454d226983f036392ac3b2eb31166d6a0987
Александр Чаусов <chausov@surfstudio.ru>
2021-04-13 15:33:40 +0300

    created example project

---
+++ Example/SurfPlaybookExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurfPlaybookExample
+//
+//  Created by Александр Чаусов on 13.04.2021.
+//  Copyright © 2021 Surf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Example/SurfPlaybookExampleTests/SurfPlaybookExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SurfPlaybookExampleTests.swift
+//  SurfPlaybookExampleTests
+//
+//  Created by Александр Чаусов on 13.04.2021.
+//  Copyright © 2021 Surf. All rights reserved.
+//
+
+import XCTest
+@testable import SurfPlaybookExample
+
+class SurfPlaybookExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
