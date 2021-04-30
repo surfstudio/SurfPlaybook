84f3bf6c60ab04f22e1f94f41d40201a63293ce2
Александр Чаусов <chausov@surfstudio.ru>
2021-04-30 18:24:28 +0300

    created test project for SPM

---
+++ SPM_Example/SurfPlaybookSPM/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  SurfPlaybookSPM
+//
+//  Created by Александр Чаусов on 30.04.2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Properties
+
+    var window: UIWindow?
+
+    // MARK: - UIApplicationDelegate
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
