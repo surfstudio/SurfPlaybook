c8004b8ac4aba252d0ae15dfeaa4bae78ebeffeb
Александр Чаусов <chausov@surfstudio.ru>
2021-05-04 17:58:02 +0300

    added comment

--- SurfPlaybook/Library/Extensions/UIKit/UIApplication.swift
+++ SurfPlaybook/Library/Extensions/UIKit/UIApplication.swift
@@ -10,6 +10,14 @@ import UIKit
 
 extension UIApplication {
 
+    /// Возвращает "топовый" viewController в текущей иерархии приложения
+    ///
+    /// Под капотом происходит рекурсивный вызов данного метода,
+    /// логика выполнения такова, что ищется navigation/tabBar-контроллеры
+    /// или модалка, если находится - то метод вызывается рекурсивно, на вход подается
+    /// найденные navigation/tabBar-контроллеры или же модалка.
+    /// Если же ничего из вышеперечисленного не нашлось - то возвращается UIViewController,
+    /// поданый на вход
     static func topViewController(_ controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
         if let navigationController = controller as? UINavigationController {
             return topViewController(navigationController.visibleViewController)
