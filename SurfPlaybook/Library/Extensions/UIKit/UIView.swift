e51cbf32e2712ed508f85cefa644d80dca3a94b2
Nikita Korobeinikov <nikita.korobeinikov@devexperts.com>
2025-01-23 17:55:22 +0400

    add extension for snapshots of SUI views

--- SurfPlaybook/Library/Extensions/UIKit/UIView.swift
+++ SurfPlaybook/Library/Extensions/UIKit/UIView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 extension UIView {
 
@@ -54,6 +55,26 @@ extension UIView {
             view.trailingAnchor.constraint(equalTo: trailingAnchor),
             view.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+    }
+
+}
+
+@available(iOS 13.0, *)
+extension View {
+
+    func snapshot() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        let view = controller.view
+
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
     }
 
 }
